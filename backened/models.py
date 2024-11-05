87967c153839c754dc765fc3004749fb9746184a
Ayan Abbas <ayanabbas727@gmail.com>
2024-11-05 22:11:51 +0530

    Experiment with models

---
+++ backened/models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class AnalysisHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    result = db.Column(db.String(50), nullable=False)
+    confidence = db.Column(db.Float, nullable=False)
